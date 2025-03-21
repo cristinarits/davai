0c554c9f5d51059ffa4f2535eab9c4b5a8635146
Cristina Rits <crystal@tlu.ee>
2025-03-21 13:52:16 +0200

    feat: integrate supabase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return(
     <div>
-      <h1 className="text-4xl font-bold text-red-900"> This is Scribe</h1>
-      <p className="mt-4 text-lg text-red-950">I have no idea what i'm doing</p>
+      <h1 className="text-4xl font-bold text-white"> This is davai</h1>
+      <p className="mt-4 text-lg text-white">and this is albert</p>
 
       <Image 
-        src = "/photo1.jpg"
+        src = "https://cpyxqewlkqmiuwwmlzaz.supabase.co/storage/v1/object/public/pictures//albert.jpg"
         alt = "that's me btw"
         width={300}
         height={300}
